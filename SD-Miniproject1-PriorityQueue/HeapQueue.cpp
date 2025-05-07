a5ac678e2357691ed264fc2aed2f177667fa2ee3
280051hubert <280051@student.pwr.edu.pl>
2025-05-07 18:31:02 +0200

    Heap Queue methods

--- SD-Miniproject1-PriorityQueue/HeapQueue.cpp
+++ SD-Miniproject1-PriorityQueue/HeapQueue.cpp
@@ -1,19 +1,28 @@
 #include "HeapQueue.hpp"
 
 template <typename T>
-HeapQueue<T>::HeapQueue(int initialCapacity) : capacity(initialCapacity), count(0)
+HeapQueue<T>::HeapQueue(int initialCapacity)
+    : capacity(initialCapacity), count(0)
 {
-    //to be done
+    heap = new Node[capacity];
 }
 
 template <typename T>
 HeapQueue<T>::~HeapQueue() {
-    //to be done
+    delete[] heap;
 }
 
 template <typename T>
 void HeapQueue<T>::resize() {
-    //to be done
+    capacity *= 2;
+    Node* newHeap = new Node[capacity];
+    for (int i = 0; i < count; ++i) 
+    {
+        newHeap[i] = heap[i];
+    }
+
+    delete[] heap;
+    heap = newHeap;
 }
 
 template <typename T>
@@ -25,7 +34,14 @@ void HeapQueue<T>::swapNodes(Node& a, Node& b) {
 
 template <typename T>
 void HeapQueue<T>::heapifyUp(int index) {
-    //to be done
+    while (index > 0) {
+
+        int parent = (index - 1) / 2;
+        if (heap[index].priority <= heap[parent].priority)
+            break;
+
+        swapNodes(heap[index], heap[parent]);
+        index = parent;
     }
 }
 
@@ -33,33 +49,74 @@ template <typename T>
 void HeapQueue<T>::heapifyDown(int index) {
     int left, right, largest;
     while (true) {
-        //to be done
+        left = 2 * index + 1;
+        right = 2 * index + 2;
+        largest = index;
+
+        if (left < count && heap[left].priority > heap[largest].priority) largest = left;
+        if (right < count && heap[right].priority > heap[largest].priority) largest = right;
+
+        if (largest == index) break;
+
+        swapNodes(heap[index], heap[largest]);
+        index = largest;
     }
 }
 
 template <typename T>
 int HeapQueue<T>::findIndex(const T& element) const {
-    //to be done
+    for (int i = 0; i < count; ++i) {
+        if (heap[i].element == element) return i;
+    }
+    return -1;
 }
 
 template <typename T>
 void HeapQueue<T>::insert(const T& element, int priority) {
-    //to be done
+    if (count == capacity) {
+        resize();
+    }
+    heap[count] = Node{ element, priority };
+    heapifyUp(count);
+    ++count;
 }
 
 template <typename T>
 T HeapQueue<T>::extractMax() {
-    //to be done
+    if (count == 0) {
+        throw std::out_of_range("HeapQueue is empty");
+    }
+    T maxElem = heap[0].element;
+    heap[0] = heap[count - 1];
+    --count;
+    if (count > 0) {
+        heapifyDown(0);
+    }
+    return maxElem;
 }
 
 template <typename T>
 T HeapQueue<T>::peekMax() const {
-    //to be done
+    if (count == 0) {
+        throw std::out_of_range("HeapQueue is empty");
+    }
+    return heap[0].element;
 }
 
 template <typename T>
 void HeapQueue<T>::modifyKey(const T& element, int newPriority) {
-//to be done
+    int index = findIndex(element);
+    if (index == -1) {
+        throw std::invalid_argument("Element not found in HeapQueue");
+    }
+    int oldPriority = heap[index].priority;
+    heap[index].priority = newPriority;
+    if (newPriority > oldPriority) {
+        heapifyUp(index);
+    }
+    else if (newPriority < oldPriority) {
+        heapifyDown(index);
+    }
 }
 
 template <typename T>
